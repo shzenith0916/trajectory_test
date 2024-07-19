80b4620a2500962d54b4ec09fcaa13319c65d134
SoHyun Kim <shkim@rsrehab.co.kr>
2024-07-19 13:57:44 +0900

    Update the trim loop code

--- files_for_yolov5/trim_loop.py
+++ files_for_yolov5/trim_loop.py
@@ -4,33 +4,51 @@ import os
 import ffmpeg
 import pandas as pd
 
-# %pip install openpyxl
+def rename_files(directory):
+    if not os.path.exists(directory):
+        print(f"Directory not found: {directory}")
+        return
+    
+    for file in os.listdir(directory):
+        old_name = os.path.join(directory, file)        
+        new_name = os.path.join(directory, file.replace(" ", ''))
+        os.rename(old_name, new_name)
+        print(f"Renamed: {old_name} to {new_name}")
 
-
-# ffmpeg -i input.mp4 -ss 00:04:40 -to 00:04:50 -vcodec -acodec output.mp4
 
 def trim_video(input_file, output_file, start, end):
-    (
-        ffmpeg
-        .input(input_file, ss=start, to=end)
-        # 'codec="copy"' applies to both video and audio. Work as the line -vcodec -acodec in FFmpeg.
-        # Without the codec line, the video quality is not assured.
-        .output(output_file, codec="copy")
-        .run(capture_stdout=True, capture_stderr=True)
-    )
+    try:
+        (
+            ffmpeg
+            .input(input_file, ss=start, to=end)
+            .output(output_file, codec="copy")
+            .run()
+        )
+        print(f"Trimmed video saved as {output_file}")
+    except ffmpeg.Error as e:
+        print(f"Failed to trim video {input_file}. Error: {e}")
+
 
 
 if __name__ == '__main__':
-    excel_path = "//mnt/c//Users//USER//Downloads//ffmpeg//VFSS_video_list_김소현_궤적용.xlsx"
-    xlsx_data = pd.read_excel(excel_path, engine='openpyxl', sheet_name='VFSS_2023년6월')
-    df = pd.DataFrame(xlsx_data)
-
-    for index, row in df.iterrows():
+    excel_path = "//mnt/c/Users/USER/Downloads/ffmpeg/VFSS_video_list_김소현_궤적용.xlsx"
+    try:
+        xlsx_data = pd.read_excel(excel_path, engine='openpyxl', sheet_name='VFSS_2023년5월')
+        df = pd.DataFrame(xlsx_data)
         
-
-
-    file_name, file_extension = os.path.splitext(input_file)
-    output_file = file_name + "_trimmed" + file_extension
-
-    # Call the method
-    trim_video(input_file, output_file, st, et)
+        video_path = "//mnt/c/Users/USER/Downloads/ffmpeg"
+        rename_files(video_path)
+        
+        for index, row in df.iterrows():
+            video_name = row["영상파일명"].replace(" ", "")
+            start_second = row["삼킴_시작(초)"]
+            end_second = row["삼킴_끝(초)"]
+            
+            input_v = os.path.join(video_path, video_name)
+            file_name, file_extension = os.path.splitext(input_v)
+            output_v = file_name + "_trimmed" + file_extension
+            
+            trim_video(input_v, output_v, start_second, end_second)
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
