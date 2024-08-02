81774fbde7e1930f7eea25bf8f9c2ef0fb8e8336
SoHyun Kim <shkim@rsrehab.co.kr>
2024-08-02 14:56:38 +0900

    Updated the path part with os.getcwd()

--- files_for_yolov5/trim_loop.py
+++ files_for_yolov5/trim_loop.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 def rename_files(directory):
     if not os.path.exists(directory):
+        print(f"Directory not found: {directory}")
         return
     
     for file in os.listdir(directory):
@@ -32,16 +33,16 @@ def trim_video(input_file, output_file, start, end):
 if __name__ == '__main__':
     excel_path = os.path.join(os.getcwd(),"VFSS_video_list_김소현_궤적용.xlsx")
     try:
-        xlsx_data = pd.read_excel(excel_path, engine='openpyxl', sheet_name='VFSS_2023년월')
+        xlsx_data = pd.read_excel(excel_path, engine='openpyxl', sheet_name='VFSS_2023년5월') # Here you declare which excel sheet you want to refer
         df = pd.DataFrame(xlsx_data)
         
-        video_path = "//mnt/c/Users/USER/Downloads/ffmpeg"
+        video_path = os.path.join(os.getcwd(),"23년5월") # Here you modify the folder name inside " "
         rename_files(video_path)
         
         for index, row in df.iterrows():
-            video_name = row["영상파일명"].replace(" ", "")
-            start_second = row["삼킴_시작(초)"]
-            end_second = row["삼킴_끝(초)"]
+            video_name = row["영상파일명"].replace(" ", "") # Modify the reference column name
+            start_second = row["삼킴_시작(초)"] # Modify the reference column name
+            end_second = row["삼킴_끝(초)"] # Modify the reference column name
             
             input_v = os.path.join(video_path, video_name)
             file_name, file_extension = os.path.splitext(input_v)
